771b54706e1c20c8654e85a0e8b25fb95d88a200
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-05-20 19:56:16 +0500

    Initial Commit

---
+++ iOS-UI-homeworks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-UI-homeworks
+//
+//  Created by Iuliia Volkova on 20.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
